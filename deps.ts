e221f56f8c3e28753cdc12e463fa28cac1890ac8
so1ve <nn_201312@163.com>
2022-03-12 17:27:45 +0800

    chore: pin dep version and type

--- deps.ts
+++ deps.ts
@@ -2,6 +2,6 @@
 export { default as AV } from "https://esm.sh/leancloud-storage@4.12.0?target=es2021&pin=v68"; // Deno deploy环境下esm.sh会默认把target设置为deno导致报错"TypeError: Can not modify env vars during execution."
 export * as leanAdapters from "https://esm.sh/@leancloud/platform-adapters-node@1.5.2?target=es2021&pin=v68";
 // Deta
-export { Base as DetaBase, Deta } from "https://esm.sh/deta@1.1.0?pin=v68";
+export { Base as DetaBase, Deta } from "https://esm.sh/deta@1.1.0?target=deno&pin=v68";
 // Lodash
 export * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
