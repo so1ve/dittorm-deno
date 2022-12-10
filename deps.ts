39aae91f50d8c25d75e33b7cf3050974151d1550
ʀᴀʏ <nn_201312@163.com>
2022-12-10 12:56:06 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // Leancloud Storage
 export { default as AV } from "https://esm.sh/leancloud-storage@4.12.0?target=es2021"; // Deno deploy环境下esm.sh会默认把target设置为deno导致报错"TypeError: Can not modify env vars during execution."
-export * as leanAdapters from "https://esm.sh/@leancloud/platform-adapters-node@1.5.2?target=es2021&pin=v68";
+export * as leanAdapters from "https://esm.sh/@leancloud/platform-adapters-node@1.5.2?target=es2021";
 // Deta
 export {
   Base as DetaBase,
